da3ed42880cf7b16aa1d02a4f5e99ed18b41e82d
Eduardo Rodríguez <74318590+earodb@users.noreply.github.com>
2020-11-12 16:57:55 -0600

    Add files via upload

---
+++ autos.php
@@ -0,0 +1,79 @@
+<?php
+require_once 'clases/respuestas.class.php';
+require_once 'clases/autos.class.php';
+
+$_respuestas = new respuestas;
+$_autos = new autos;
+
+
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+
+    if(isset($_GET['id'])){
+        $autoid = $_GET['id'];
+        $datosAuto = $_autos->obtenerAuto($autoid);
+        header("Content-Type: application/json");
+        echo json_encode($datosAuto);
+        http_response_code(200);
+    }else{
+        $listaAutos = $_autos->listaAutos(); 
+        header("Content-Type: application/json");
+        echo json_encode($listaAutos);
+        http_response_code(200);
+    }
+    
+}else if($_SERVER['REQUEST_METHOD'] == "POST"){
+    //recibimos los datos enviados
+    $postBody = file_get_contents("php://input");
+    //enviamos los datos al manejador
+    $datosArray = $_autos->post($postBody);
+
+    //delvovemos una respuesta 
+     header('Content-Type: application/json');
+     if(isset($datosArray["result"]["error_id"])){
+         $responseCode = $datosArray["result"]["error_id"];
+         http_response_code($responseCode);
+     }else{
+         http_response_code(200);
+     }
+     echo json_encode($datosArray); 
+  
+}
+else if($_SERVER['REQUEST_METHOD'] == "PUT"){
+      //recibimos los datos enviados
+     $postBody = file_get_contents("php://input");
+      //enviamos datos al manejador
+      $datosArray = $_autos->put($postBody);
+        //delvovemos una respuesta 
+      header('Content-Type: application/json');
+     if(isset($datosArray["result"]["error_id"])){
+         $responseCode = $datosArray["result"]["error_id"];
+         http_response_code($responseCode);
+     }else{
+         http_response_code(200);
+     }
+     echo json_encode($datosArray); 
+
+}else if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+
+    $postBody = file_get_contents("php://input");
+    //enviamos datos al manejador
+    $datosArray = $_autos->delete($postBody);
+    //delvovemos una respuesta 
+       header('Content-Type: application/json');
+     if(isset($datosArray["result"]["error_id"])){
+         $responseCode = $datosArray["result"]["error_id"];
+         http_response_code($responseCode);
+     }else{
+         http_response_code(200);
+     }
+     echo json_encode($datosArray); 
+
+       
+}else{
+    header('Content-Type: application/json');
+    $datosArray = $_respuestas->error_405();
+    echo json_encode($datosArray);
+}
+
+
+?>
